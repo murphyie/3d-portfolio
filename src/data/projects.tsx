a96f57ea800b92a3274433013bfa6d48bc8d00a0
murphyie <murphyiee@outlook.com>
2026-03-10 00:59:07 +0530

    Update projects.tsx

--- src/data/projects.tsx
+++ src/data/projects.tsx
@@ -1,67 +1,42 @@
-import AceTernityLogo from "@/components/logos/aceternity";
-import SlideShow from "@/components/slide-show";
-import { Button } from "@/components/ui/button";
-import { TypographyH3, TypographyP } from "@/components/ui/typography";
-import { ArrowUpRight, ExternalLink, Link2, MoveUpRight } from "lucide-react";
-import Image from "next/image";
-import Link from "next/link";
 import { ReactNode } from "react";
-import { RiNextjsFill, RiNodejsFill, RiReactjsFill } from "react-icons/ri";
-import {
-  SiChakraui,
-  SiDocker,
-  SiExpress,
-  SiFirebase,
-  SiJavascript,
-  SiMongodb,
-  SiPostgresql,
-  SiPrisma,
-  SiPython,
-  SiReactquery,
-  SiSanity,
-  SiShadcnui,
-  SiSocketdotio,
-  SiSupabase,
-  SiTailwindcss,
-  SiThreedotjs,
-  SiTypescript,
-  SiVuedotjs,
-} from "react-icons/si";
-import { TbBrandFramerMotion } from "react-icons/tb";
-const BASE_PATH = "/assets/projects-screenshots";
 
-const ProjectsLinks = ({ live, repo }: { live: string; repo?: string }) => {
-  return (
-    <div className="flex flex-col md:flex-row items-center justify-start gap-3 my-3 mb-8">
-      <Link
-        className="font-mono underline flex gap-2"
-        rel="noopener"
-        target="_new"
-        href={live}
-      >
-        <Button variant={"default"} size={"sm"}>
-          Visit Website
-          <ArrowUpRight className="ml-3 w-5 h-5" />
-        </Button>
-      </Link>
-      {repo && (
-        <Link
-          className="font-mono underline flex gap-2"
-          rel="noopener"
-          target="_new"
-          href={repo}
-        >
-          <Button variant={"default"} size={"sm"}>
-            Github
-            <ArrowUpRight className="ml-3 w-5 h-5" />
-          </Button>
-        </Link>
-      )}
-    </div>
-  );
-};
 export type Project = {
+  title: string;
+  src: string;
+  type: "image" | "video";
 };
+
 const projects: Project[] = [
+  {
+    title: "3D Portfolio",
+    src: "/assets/projects-screenshots/project1.png",
+    type: "image",
+  },
+  {
+    title: "Landing Page Design",
+    src: "/assets/projects-screenshots/project2.png",
+    type: "image",
+  },
+  {
+    title: "Dashboard UI",
+    src: "/assets/projects-screenshots/project3.png",
+    type: "image",
+  },
+  {
+    title: "Product Animation",
+    src: "/assets/projects-screenshots/video1.mp4",
+    type: "video",
+  },
+  {
+    title: "Website Interaction Demo",
+    src: "/assets/projects-screenshots/video2.mp4",
+    type: "video",
+  },
+  {
+    title: "App Preview",
+    src: "/assets/projects-screenshots/video3.mp4",
+    type: "video",
+  },
 ];
+
 export default projects;
